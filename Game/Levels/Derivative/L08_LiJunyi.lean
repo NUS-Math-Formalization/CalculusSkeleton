c5c4205f2665050e919e96724126b7b56027fe1b
Huanchen <huanchen@nus.edu.sg>
2024-09-21 11:33:02 +0800

    create sections

---
+++ Game/Levels/Derivative/L08_LiJunyi.lean
@@ -0,0 +1,13 @@
+import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.Analysis.Calculus.Deriv.Inv
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Deriv
+import Mathlib.Analysis.SpecialFunctions.Pow.Real
+import Mathlib.Data.Complex.Exponential
+import Mathlib.Tactic
+import Game.Metadata
+
+World "Derivative"
+
+Level 8
+
+Title "TBA"
---
+++ Game/Levels/Derivative/L09_KunYu.lean
@@ -0,0 +1,22 @@
+import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.Tactic
+import Game.Metadata
+
+World "Derivative"
+
+Level 9
+
+Title "Introduction to Derivative"
+
+open Real
+
+Statement (x : ℝ) : deriv (fun x : ℝ => x) (x : ℝ) = 1 := by
+  Hint "Differentiation of the identity map on ℝ is a known result in Mathlib4, so you can take advantage of this lemma, which is called `deriv_id''`. Besure to check its statement in the right panel. Use tatic `rw` to rewrite the goal or `apply` to apply the lemma."
+  rw [deriv_id'']
+
+NewTactic rw apply
+
+/-- The derivative of Identity function $f(x) = x$ on ℝ is $1$ -/
+TheoremDoc deriv_id'' as "deriv_id''" in "Derivative"
+
+NewTheorem deriv_id''
