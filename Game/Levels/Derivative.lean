ac646df31799ae5fc6224230c83e3a02868a5700
Huanchen <huanchen@nus.edu.sg>
2024-09-21 11:24:05 +0800

    update sections

--- Game/Levels/Derivative.lean
+++ Game/Levels/Derivative.lean
@@ -4,6 +4,11 @@ import Game.Levels.Derivative.L03_Square
 import Game.Levels.Derivative.L04_ProductRule
 import Game.Levels.Derivative.L05_EXpowerEX
 import Game.Levels.Derivative.L06_QuotientRule
+import Game.Levels.Derivative.L07_FanXinyuan
+import Game.Levels.Derivative.L08_LiJunyi
+import Game.Levels.Derivative.L09_KunYu
+import Game.Levels.Derivative.L10_PangBo
+import Game.Levels.Derivative.L11_DanielLow
 
 
 World "Derivative"
