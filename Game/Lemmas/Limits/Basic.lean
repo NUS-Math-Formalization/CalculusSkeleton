2c1cee72ebe058589929619bf8a0cb1882573ffb
Chong Jing Quan <jingquan0302@gmail.com>
2024-10-07 14:11:01 +0000

    one-sided limits

--- Game/Lemmas/Limits/Basic.lean
+++ Game/Lemmas/Limits/Basic.lean
@@ -7,24 +7,40 @@ open Filter Set Classical
 
 noncomputable section LimDef
 
+-- to fix: change to functions defined on intervals
 def HasLimAt (f : ℝ → ℝ) (c : ℝ) := ∃ (l₂ : ℝ), Tendsto f (nhdsWithin c {c}ᶜ) (nhds l₂)
 
+def HasLeftLimAt (f : ℝ → ℝ) (c : ℝ) := ∃ (l₂ : ℝ), Tendsto f (nhdsWithin c (Set.Iio c)) (nhds l₂)
+
+def HasRightLimAt (f : ℝ → ℝ) (c : ℝ) := ∃ (l₂ : ℝ), Tendsto f (nhdsWithin c (Set.Ioi c)) (nhds l₂)
+
 def HasLimAtTop (f : ℝ → ℝ) := ∃ (l₂ : ℝ), Tendsto f atTop (nhds l₂)
+
+-- add HasLimAtBot
 
 irreducible_def flim (f : ℝ → ℝ) (l₁ : Filter ℝ) : ℝ :=
   if h : ∃ L, Tendsto f l₁ (nhds L) then h.choose else 0
 
--- irreducible_def flim
+-- irreducible_def flim (add lim at bot as well as lim to bot)
 syntax "lim " term:40 " → " term:10 ", " term:70: term
 syntax "lim " term:40 " → ∞, " term:70: term
 syntax "lim " term:40 " → " term:10 ", " term:70 " = ∞": term
 syntax "lim " term:40 " → ∞, " term:70 " = ∞": term
+syntax "lim " term:40 " → " term:10 "⁺ , " term:70: term
+syntax "lim " term:40 " → " term:10 "⁻ , " term:70: term
+syntax "lim " term:40 " → " term:10 "⁺ , " term:70 " = ∞": term
+syntax "lim " term:40 " → " term:10 "⁻ , " term:70 " = ∞": term
 
 macro_rules
   | `(lim $x → ∞, $r = ∞) => `(Tendsto (fun $x => $r) atTop atTop)
   | `(lim $x → $c, $r) => `(flim (fun $x => $r) (nhdsWithin $c {($c)}ᶜ))
   | `(lim $x → ∞, $r) =>  `(flim (fun $x => $r) atTop)
   | `(lim $x → $c, $r = ∞) => `(Tendsto (fun $x => $r) (nhdsWithin $c {($c)}ᶜ) atTop)
+  | `(lim $x → $c⁺ , $r) => `(flim (fun $x => $r) (nhdsWithin $c (Set.Ioi $c)))
+  | `(lim $x → $c⁻ , $r) => `(flim (fun $x => $r) (nhdsWithin $c (Set.Iio $c)))
+  | `(lim $x → $c⁻ , $r = ∞) => `(Tendsto (fun $x => $r) (nhdsWithin $c (Set.Iio $c)) atTop)
+  | `(lim $x → $c⁺ , $r = ∞) => `(Tendsto (fun $x => $r) (nhdsWithin $c (Set.Ioi $c)) atTop)
+
 
 
 variable {c L : ℝ} {f : ℝ → ℝ}
@@ -33,8 +49,7 @@ lemma nhds_basis_abs_sub_lt_deleted (a : ℝ) :
     (nhdsWithin a {a}ᶜ).HasBasis (fun ε : ℝ => 0 < ε) fun ε => { b | 0 < |b - a| ∧ |b - a| < ε }
     := by
   have : (fun ε => { b | 0 < |b - a| ∧ |b - a| < ε }) = (fun ε => {b | |b - a| < ε} ∩ {a}ᶜ) := by
-    funext ε
-    ext x
+    funext ε; ext x
     simp only [mem_inter_iff, mem_setOf_eq, mem_compl_iff, mem_singleton_iff, abs_pos, ne_eq]
     rw [and_comm]
     simp only [and_congr_right_iff]
@@ -43,9 +58,27 @@ lemma nhds_basis_abs_sub_lt_deleted (a : ℝ) :
   rw [this]
   apply nhdsWithin_hasBasis (nhds_basis_abs_sub_lt (α := ℝ) a) ({a}ᶜ)
 
+-- uhh idk how to prove this :(
+lemma nhds_basis_left (a : ℝ) :
+  (nhdsWithin a (Set.Iio a)).HasBasis (fun ε : ℝ => 0 < ε) fun ε => { b | 0 < a - b ∧ a - b < ε }
+  := by
+  have : (fun ε => { b | 0 < a - b ∧ a - b < ε }) = (fun ε => Set.Ioo (a - ε) a) := by
+    funext ε; ext x
+    simp only [sub_pos, mem_setOf_eq, mem_Ioo]
+    rw [and_comm]
+    simp only [and_congr_left_iff]
+    intro
+    exact sub_lt_comm
+  rw [this]
+  have ord : ∃ b, b < a := by use (a - 1); norm_num
+  have : (HasBasis (nhdsWithin a (Iio a)) (fun x => x < a) fun x => Ioo x a) ↔
+    HasBasis (nhdsWithin a (Iio a)) (fun ε => 0 < ε) fun ε => Ioo (a - ε) a := by
+    sorry
+  rw [← this]
+  apply nhdsWithin_Iio_basis' (α := ℝ) ord
 
-lemma epsilon_delta_nhds_nhds_deleted (f : ℝ → ℝ) (c L : ℝ) :
-  Tendsto f (nhdsWithin c {c}ᶜ) (nhds L) ↔
+
+lemma epsilon_delta_nhds_nhds_deleted : Tendsto f (nhdsWithin c {c}ᶜ) (nhds L) ↔
   ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < |x - c| ∧ |x - c| < δ → |f x - L| < ε := by
   have NHBD := nhds_basis_abs_sub_lt_deleted c
   have NHB := nhds_basis_abs_sub_lt (α := ℝ)
@@ -58,6 +91,56 @@ lemma lim_def_fin_fin (h : ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < |x - c| ∧ |x - c
   have hL : ∃ L, Tendsto f (nhdsWithin c {c}ᶜ) (nhds L) := ⟨L, h⟩
   rw [flim, dif_pos hL]
   exact tendsto_nhds_unique hL.choose_spec h
+
+
+lemma epsilon_delta_nhds_nhds_left : Tendsto f (nhdsWithin c (Set.Iio c)) (nhds L) ↔
+  ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < c - x ∧ c - x < δ → |f x - L| < ε := by
+  have : ∃ b, b < c := by use (c - 1); norm_num
+  have NHBL := nhdsWithin_Iio_basis' (α := ℝ) this
+  have NHB := nhds_basis_abs_sub_lt (α := ℝ)
+  simp_rw [HasBasis.tendsto_iff (NHBL) (NHB L), mem_setOf]
+  simp only [mem_Ioo, and_imp, gt_iff_lt, sub_pos]
+  constructor
+  . intro h ε εpos
+    have : ∃ ia < c, ∀ x, ia < x → x < c → |f x - L| < ε := by apply h; exact εpos
+    rcases this with ⟨ia, iapos, iah⟩
+    use (c - ia)
+    constructor
+    . linarith
+    . intro h₁ h₂ h₃
+      apply iah; linarith; linarith
+  . intro h ε εpos
+    have : ∃ δ, 0 < δ ∧ (∀ x, x < c → c - x < δ → |f x - L| < ε) := by apply h; exact εpos
+    rcases this with ⟨δ, δpos, δh⟩
+    use (c - δ)
+    constructor
+    . linarith
+    . intro u u₁ u₂
+      apply δh; linarith; linarith
+
+/--
+lemma epsilon_delta_nhds_nhds_left' (f : ℝ → ℝ) (c L : ℝ) :
+  Tendsto f (nhdsWithin c (Set.Iio c)) (nhds L) ↔
+  ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < c - x ∧ c - x < δ → |f x - L| < ε := by
+  have NHBL := nhds_basis_left c
+  have NHB := nhds_basis_abs_sub_lt (α := ℝ)
+  simp_rw [HasBasis.tendsto_iff (NHBL) (NHB L), mem_setOf_eq]
+-/
+
+lemma left_lim_def_fin_fin (h : ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < c - x ∧ c - x < δ → |f x - L| < ε) :
+  lim x → c⁻ , f x = L := by
+  rw [← epsilon_delta_nhds_nhds_left] at h
+  have hL : ∃ L, Tendsto f (nhdsWithin c (Set.Iio c)) (nhds L) := ⟨L, h⟩
+  rw [flim, dif_pos hL]
+  exact tendsto_nhds_unique hL.choose_spec h
+
+
+lemma epsilon_delta_nhds_nhds_right : Tendsto f (nhdsWithin c (Set.Ioi c)) (nhds L) ↔
+  ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < x - c ∧ x - c < δ → |f x - L| < ε := by sorry
+
+
+lemma right_lim_def_fin_fin (h : ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < x - c ∧ x - c < δ → |f x - L| < ε) :
+  lim x → c⁺ , f x = L := by sorry
 
 
 lemma epsilon_delta_atTop_nhds : Tendsto f atTop (nhds L) ↔
@@ -86,6 +169,22 @@ lemma lim_def_fin_inf (h : ∀ N : ℝ, ∃ δ > 0, ∀ x, 0 < |x - c| ∧ |x - 
   lim x → c, f x = ∞ := epsilon_delta_nhds_atTop_deleted.mpr h
 
 
+lemma epsilon_delta_nhds_atTop_left : Tendsto f (nhdsWithin c (Set.Iio c)) atTop ↔
+  ∀ N : ℝ, ∃ δ > 0, ∀ x, 0 < c - x ∧ c - x < δ → f x > N := by sorry
+
+
+lemma left_lim_def_fin_inf (h : ∀ N : ℝ, ∃ δ > 0, ∀ x, 0 < c - x ∧ c - x < δ → f x > N) :
+  lim x → c⁻ , f x = ∞ := by sorry
+
+
+lemma epsilon_delta_nhds_atTop_right : Tendsto f (nhdsWithin c (Set.Ioi c)) atTop ↔
+  ∀ N : ℝ, ∃ δ > 0, ∀ x, 0 < x - c ∧ x - c < δ → f x > N := by sorry
+
+
+lemma right_lim_def_fin_inf (h : ∀ N : ℝ, ∃ δ > 0, ∀ x, 0 < x - c ∧ x - c < δ → f x > N) :
+  lim x → c⁺ , f x = ∞ := by sorry
+
+
 lemma epsilon_delta_atTop_atTop : Tendsto f atTop atTop ↔
   ∀ N : ℝ, ∃ M, ∀ x, x > M → f x > N := by
   have THB := atTop_basis_Ioi (α := ℝ)
@@ -96,8 +195,3 @@ lemma lim_def_inf_inf (h : ∀ N : ℝ, ∃ M, ∀ x, x > M → f x > N) :
   lim x → ∞, f x = ∞ := epsilon_delta_atTop_atTop.mpr h
 
 end LimDef
-
--- there's still one-sided limits to handle lol
--- use the following notations, again need punctured neighbourhoods
--- `𝓝[<] x`: the filter `nhdsWithin x (Set.Iio x)` of punctured left-neighborhoods of `x`;
--- `𝓝[>] x`: the filter `nhdsWithin x (Set.Ioi x)` of punctured right-neighborhoods of `x`;
