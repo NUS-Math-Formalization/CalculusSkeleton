5eb55dc66337edba1a5559dc50cc046213c6a4bb
Ma, Jia-Jun <hoxide@gmail.com>
2025-04-10 08:41:07 +0800

    fix

--- Game/Levels/Limit.lean
+++ Game/Levels/Limit.lean
@@ -1,7 +1,7 @@
 import Game.Levels.Limit.L01_Introduction
-import Game.Levels.Limit.L02_Sin
-import Game.Levels.Limit.L03_Add
-import Game.Levels.Limit.L04_Linear
+import Game.Levels.Limit.L021_group
+--import Game.Levels.Limit.L03_Add
+--import Game.Levels.Limit.L04_Linear
 -- import Game.Levels.Limit.L05_PangBo
 -- import Game.Levels.Limit.L06_KunYu
 -- import Game.Levels.Limit.L07_LiJunyi
